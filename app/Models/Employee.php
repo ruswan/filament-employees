840e6f8f268b1c871b8197701f8726b49e35be17
ruswan <sukabumikode@gmail.com>
2023-05-08 09:06:26 +0700

    Add model, migration and relation

---
+++ app/Models/Employee.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'address',
+        'city_id',
+        'state_id',
+        'country_id',
+        'department_id',
+        'zip_code',
+        'birth_date',
+        'date_hired'
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+}
